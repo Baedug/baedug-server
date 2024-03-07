e197b68f6455f936b1f3843e9d55377fa7d8af5d
nyeroni <nir2y@naver.com>
2024-03-07 19:55:16 +0900

    Feat: 애플로그인 구현

--- src/main/java/yerong/baedug/domain/member/Member.java
+++ src/main/java/yerong/baedug/domain/member/Member.java
@@ -32,7 +32,7 @@ public class Member extends BaseTimeEntity {
     @Column(nullable = false)
     private String socialId;
 
-    @Column(nullable = false)
+    //@Column(nullable = false)
     private String username;
 
     @Column(nullable = false)
