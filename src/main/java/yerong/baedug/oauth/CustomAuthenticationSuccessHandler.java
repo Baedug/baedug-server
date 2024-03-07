aacb0b94d2f9873c066348bec3bd929d59b31c6c
nyeroni <nir2y@naver.com>
2024-03-07 17:14:33 +0900

    Feat: 애플로그인 구현

--- src/main/java/yerong/baedug/oauth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/yerong/baedug/oauth/CustomAuthenticationSuccessHandler.java
@@ -11,12 +11,18 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Req
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    @Value("${social-login.provider.apple.redirect-uri}")
+
     private String redirectUrl;
+
+    public CustomAuthenticationSuccessHandler(@Value("${social-login.provider.apple.redirect-uri}") String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.sendRedirect(redirectUrl); // 리다이렉션할 URI 설정
+        response.sendRedirect(redirectUrl);
     }
 }
