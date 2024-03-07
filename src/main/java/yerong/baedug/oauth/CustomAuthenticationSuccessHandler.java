6c6c727967bb32068a3bebece27682ce79f4bb51
nyeroni <nir2y@naver.com>
2024-03-07 17:16:45 +0900

    Feat: 오타 수정

--- src/main/java/yerong/baedug/oauth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/yerong/baedug/oauth/CustomAuthenticationSuccessHandler.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-@Req
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
 
