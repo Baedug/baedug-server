bc1d7a26eb67c51bcef168248e41249e06648cde
nyeroni <nir2y@naver.com>
2024-05-16 15:47:11 +0900

    feat: swagger 설정 및 예외처리 수정

--- src/main/java/yerong/baedug/directory/dto/response/DirectoryResponseDto.java
+++ src/main/java/yerong/baedug/directory/dto/response/DirectoryResponseDto.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import yerong.baedug.directory.domain.Directory;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class DirectoryResponseDto {
 
+    private Long id;
     private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public DirectoryResponseDto(Directory directory){
+        this.id = directory.getId();
         this.name = directory.getName();
+        this.createdAt = directory.getCreatedAt();
+        this.updatedAt = directory.getUpdatedAt();
     }
 }
